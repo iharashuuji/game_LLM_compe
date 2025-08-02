512be7166992bb43deb82be31bbaaffb1b0ce363
iharashuuji <84663648+iharashuuji@users.noreply.github.com>
2025-08-02 11:43:44 +0900

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 score=0
 
-if [ -d "starter/workspace/report" ]; then
+if [ -d "./workspace/report" ]; then
   echo "✅ workspace/report ディレクトリ: OK"
   score=$((score+1))
 else
   echo "❌ workspace/report ディレクトリ: NG"
 fi
 
-if grep -q "Line 1" starter/workspace/report/report.txt 2>/dev/null; then
+if grep -q "Line 1" ./workspace/report/report.txt 2>/dev/null; then
   echo "✅ report.txt 内容: OK"
   score=$((score+1))
 else
